2936c90a3d47949487f627b33147d059a1050546
Jan Bzdyl <94219390+defcon1one1@users.noreply.github.com>
2023-03-05 18:17:25 +0100

    Add files via upload

---
+++ AI Art Site/AI-Art-Website-main/AI Art Site/app.js
@@ -0,0 +1,62 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+})
+
+
+window.onload = () => {
+    const textHighlight = document.querySelector('.text-highlight');
+    const textChars = textHighlight.textContent.split('');
+    
+    
+    textHighlight.innerHTML = '';
+    textChars.forEach(char => {
+      const span = document.createElement('span');
+      span.textContent = char;
+      textHighlight.appendChild(span);
+    });
+    
+    
+    const textSpans = textHighlight.querySelectorAll('span');
+    textSpans.forEach(span => {
+      span.style.opacity = 0;
+      span.style.transform = 'translateY(100%)';
+    });
+    
+    
+    let index = 0;
+    const interval = setInterval(() => {
+      const span = textSpans[index];
+      span.style.opacity = 1;
+      span.style.transform = 'translateY(0)';
+      index++;
+      if (index === textSpans.length) {
+        clearInterval(interval);
+      }
+    }, 35);
+
+};
+
+const mainImg = document.querySelector('#main__img');
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    mainImg.classList.add('show');
+  }, 500);
+});
+
+
+const navbar = document.querySelector('.navbar');
+const navbarHeight = navbar.offsetHeight;
+const mainSection = document.querySelector('.main');
+const mainSectionOffsetTop = mainSection.offsetTop;
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > mainSectionOffsetTop - navbarHeight) {
+    navbar.classList.add('navbar--scrolled');
+  } else {
+    navbar.classList.remove('navbar--scrolled');
+  }
+});
